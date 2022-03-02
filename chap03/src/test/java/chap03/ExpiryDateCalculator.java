697c36779055a7029839772bb0f1deffdf63c692
hyeddo <syong0716@naver.com>
2022-03-02 21:34:55 +0900

    220302

---
+++ chap03/src/test/java/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,22 @@
+package chap03;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class ExpiryDateCalculator {
+    public LocalDate calculateExpiryDate(PayData payData){
+        int addedMonths = payData.getPayAmount() / 10_000;
+        if(payData.getFirstBillingDate() != null){
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if(dayOfFirstBilling != candidateExp.getDayOfMonth()){
+                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+                if(dayLenOfCandiMon < dayOfFirstBilling){
+                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+                }
+                return candidateExp.withDayOfMonth(dayOfFirstBilling);
+            }
+        }
+        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+}
